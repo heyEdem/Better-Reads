399585f6d018cc8fcaf9d3ee4f7a65eb2068cfe3
heyEdem <akatoaugustine@gmail.com>
2023-06-08 02:07:14 +0000

    Controller and Service update

--- src/main/java/com/edem/LibraryManagementSystem/BookController.java
+++ src/main/java/com/edem/LibraryManagementSystem/BookController.java
@@ -3,12 +3,9 @@ package com.edem.LibraryManagementSystem;
 
 import com.edem.LibraryManagementSystem.Service.BookService;
 import com.edem.LibraryManagementSystem.entity.Book;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +23,25 @@ public class BookController {
     public ResponseEntity<List<Book>> getAllBooks(){
         List<Book> books= bookService.findAllBooks();
         return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity <Long> addNewBook(@RequestBody Book book){
+        Book insertedbook = bookService.createBook(book);
+        if(insertedbook == null)
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity (insertedbook.getId(), HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Book> update(@RequestBody Book book){
+        bookService.updateBook(book);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id){
+        bookService.deleteBook(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
