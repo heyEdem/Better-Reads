4ae9e09f76a81989e183c54cf2838aa5683e4984
heyEdem <akatoaugustine@gmail.com>
2023-06-11 04:37:59 +0000

    Update

--- src/main/java/com/edem/LibraryManagementSystem/Controller/BookController.java
+++ src/main/java/com/edem/LibraryManagementSystem/Controller/BookController.java
@@ -1,23 +1,61 @@
 package com.edem.LibraryManagementSystem.Controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.edem.LibraryManagementSystem.Service.BookService;
 import com.edem.LibraryManagementSystem.entity.Book;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 public class BookController {
 
-    @Autowired
-    private BookService bookService;
+    private final BookService bookService;
 
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/")
     public String getAllBooks(Model model){
         List<Book> books= bookService.findAllBooks();
         model.addAttribute("books",books);
         return "index";
+    }
+
+
+
+    @PostMapping("/addBook")
+    public String addNewBook(Book book){
+        Book result = bookService.createBook(book);
+        if(result==null){
+            return "redirect:/";
+        }
+        return "redirect:/";
+    }
+
+
+    @PutMapping
+    @RequestMapping({"/edit","edit/{id}"})
+    public String editBook(Model model, @PathVariable("id") Optional<Long> id){
+        if (id.isPresent()) {
+            Optional <Book> book = bookService.findBookById(id.get());
+            if (book.isPresent()) {
+                model.addAttribute("book",book);
+            }
+            else{
+                model.addAttribute("book", new Book());
+            }
+        }
+        return "add-edit-book";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id){
+        bookService.deleteBook(id);
+        return "redirect:/";
     }
 }
