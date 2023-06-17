883b86fff5735d86022a7648ba45a393bbe93d49
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-06-17 22:59:11 +0000

    Security

--- src/main/java/com/edem/LibraryManagementSystem/Config/SecurityConfiguration.java
+++ src/main/java/com/edem/LibraryManagementSystem/Config/SecurityConfiguration.java
@@ -1,7 +1,21 @@
 package com.edem.LibraryManagementSystem.Config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user_admin").password("{{noop}}1234").roles(UserRole.ADMIN.name())
+                .and().withUser("user_publisher").password("{{noop}}123").roles(UserRole.PUBLISHER.name())
+                .and().withUser("user_read_only").password("{{noop}}123").roles(UserRole.READ_ONLY.name());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
 }
--- src/main/java/com/edem/LibraryManagementSystem/Config/UserRole.java
+++ src/main/java/com/edem/LibraryManagementSystem/Config/UserRole.java
@@ -3,6 +3,6 @@ package com.edem.LibraryManagementSystem.Config;
 public enum UserRole {
 
     ADMIN,
-    USER,
+    READ_ONLY,
     PUBLISHER
 }
