b7b0d76e86e152684c13beb452e161169e8ac984
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-06-24 02:12:56 +0000

    Final stages

--- src/main/java/com/edem/LibraryManagementSystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/edem/LibraryManagementSystem/LibraryManagementSystemApplication.java
@@ -11,39 +11,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class LibraryManagementSystemApplication implements ApplicationRunner {
+public class LibraryManagementSystemApplication /*implements ApplicationRunner*/ {
 
-	private BookService bookService;
-
-	public LibraryManagementSystemApplication(BookService bookService) {
-		this.bookService = bookService;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementSystemApplication.class, args);
 	}
 
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-
-		List<Book> books = bookService.findAllBooks();
-
-		if (books != null && books.isEmpty()) {
-
-			Book book1 = Book.builder().title("Book1").author("Josh Long").description("Description").price(214.5)
-					.build();
-			Book book2 = Book.builder().title("Book2").author("Hannan Gambit").description("Description").price(11.8)
-					.build();
-			Book book3 = Book.builder().title("Book3").author("Elvis Ennumh").description("Description").price(21.7)
-					.build();
-			Book book4 = Book.builder().title("Book4").author("Emefa Diana").description("Description").price(3.99)
-					.build();
-
-			Arrays.asList(book1, book2, book3).forEach(b -> bookService.createBook(b));
-
-			System.out.println("New books added in database");
-		}
-	}
+//	@Override
+//	public void run(ApplicationArguments args) throws Exception {
+//
+//		List<Book> books = bookService.findAllBooks();
+//
+//		if (books != null && books.isEmpty()) {
+//
+//			Book book1 = Book.builder().title("Book1").author("Josh Long").description("Description").price(214.5)
+//					.build();
+//			Book book2 = Book.builder().title("Book2").author("Hannan Gambit").description("Description").price(11.8)
+//					.build();
+//			Book book3 = Book.builder().title("Book3").author("Elvis Ennumh").description("Description").price(21.7)
+//					.build();
+//			Book book4 = Book.builder().title("Book4").author("Emefa Diana").description("Description").price(3.99)
+//					.build();
+//
+//			Arrays.asList(book1, book2, book3).forEach(b -> bookService.createBook(b));
+//
+//			System.out.println("New books added in database");
+//		}
+//	}
 
 }
