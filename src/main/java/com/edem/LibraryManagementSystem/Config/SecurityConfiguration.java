8623b929abcb2b8f8798baacb28c1541cbbeb165
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-08-12 21:55:18 +0000

    touches

--- src/main/java/com/edem/LibraryManagementSystem/Config/SecurityConfiguration.java
+++ src/main/java/com/edem/LibraryManagementSystem/Config/SecurityConfiguration.java
@@ -1,33 +1,32 @@
-package com.edem.LibraryManagementSystem.Config;
-
-import org.apache.catalina.User;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("{noop}1234").roles(UserRole.ADMIN.name())
-                .and().withUser("publisher").password("{noop}123").roles(UserRole.PUBLISHER.name())
-                .and().withUser("user").password("{noop}12").roles(UserRole.READ_ONLY.name());
-    }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .antMatchers("/delete").hasRole(UserRole.ADMIN.name())
-                .antMatchers("/edit").hasAnyRole(UserRole.ADMIN.name(),UserRole.PUBLISHER.name())
-                .antMatchers("/actuator/**").hasRole(UserRole.ADMIN.name())
-                .anyRequest().authenticated()
-                .and()
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll());
-    }
-
-
-}
+//package com.edem.LibraryManagementSystem.Config;
+//
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("admin").password("{noop}1234").roles(UserRole.ADMIN.name())
+//                .and().withUser("publisher").password("{noop}123").roles(UserRole.PUBLISHER.name())
+//                .and().withUser("user").password("{noop}12").roles(UserRole.READ_ONLY.name());
+//    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests()
+//                .antMatchers("/delete").hasRole(UserRole.ADMIN.name())
+//                .antMatchers("/edit").hasAnyRole(UserRole.ADMIN.name(), UserRole.PUBLISHER.name())
+//                .antMatchers("/actuator/**").hasRole(UserRole.ADMIN.name())
+//                .antMatchers("/api/books/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin();
+//    }
+//
+//
+//}
