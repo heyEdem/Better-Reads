bc8a7932dee7ab1de321de1ca2ded855bc19e21c
Edem Akato <akatoaugustine@gmail.com>
2023-10-13 23:30:17 +0000

    Security Configuration

--- src/main/java/com/edem/LibraryManagementSystem/Config/SecurityConfiguration.java
+++ src/main/java/com/edem/LibraryManagementSystem/Config/SecurityConfiguration.java
@@ -3,6 +3,10 @@ package com.edem.LibraryManagementSystem.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,6 +25,19 @@ public class SecurityConfiguration {
                 )
                 .logout((logout)-> logout.permitAll());
         return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("1234")
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 
 //    @Override
