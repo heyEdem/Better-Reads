9dd3c794430241a320e21e857bdadc6213088160
Edem Akato <akatoaugustine@gmail.com>
2023-10-13 00:42:56 +0000

    Security Configuration

--- src/main/java/com/edem/LibraryManagementSystem/Config/SecurityConfiguration.java
+++ src/main/java/com/edem/LibraryManagementSystem/Config/SecurityConfiguration.java
@@ -1,14 +1,29 @@
-//package com.edem.LibraryManagementSystem.Config;
-//
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
+package com.edem.LibraryManagementSystem.Config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration {
+    private HttpSecurity http;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((requests)-> requests
+                        .requestMatchers("/","home").permital()
+                        .anyRequests().authenticated;
+                )
+        .formLogin((form)-> form
+                .loginPage("/login")
+        )
+
+    }
+
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //        auth.inMemoryAuthentication()
@@ -27,6 +42,6 @@
 //                .and()
 //                .formLogin();
 //    }
-//
-//
-//}
+
+
+}
