1abdc0b5dcedd2d3efe220e0bccb5fd65afa734d
Edem Akato <akatoaugustine@gmail.com>
2023-10-13 23:19:14 +0000

    Security Configuration

--- src/main/java/com/edem/LibraryManagementSystem/Config/SecurityConfiguration.java
+++ src/main/java/com/edem/LibraryManagementSystem/Config/SecurityConfiguration.java
@@ -1,27 +1,26 @@
 package com.edem.LibraryManagementSystem.Config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
-    private HttpSecurity http;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
         http
-                .authorizeHttpRequests((requests)-> requests
-                        .requestMatchers("/","home").permital()
-                        .anyRequests().authenticated;
+                .authorizeHttpRequests((requests)->requests
+                        .requestMatchers("/", "/api/books/**").permitAll()
+                        .anyRequest().authenticated()
                 )
-        .formLogin((form)-> form
-                .loginPage("/login")
-        )
-
+                .formLogin((form)->form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout)-> logout.permitAll());
+        return http.build();
     }
 
 //    @Override
