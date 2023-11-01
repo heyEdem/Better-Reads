0bff58020b100129fb3df30d9c12865373d18020
heyEdem <akatoaugustine@gmail.com>
2023-11-01 01:26:30 +0000

    Author Service & Repository

---
+++ src/main/java/com/edem/LibraryManagementSystem/Service/AuthorService.java
@@ -0,0 +1,15 @@
+package com.edem.LibraryManagementSystem.Service;
+
+import com.edem.LibraryManagementSystem.entity.Author;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AuthorService {
+    Author createAuthor (Author author);
+    List<Author> findAllAuthors();
+    void updateAuthor(Author author);
+    void deleteAuthor(Long authorId);
+    Optional<Author> findAuthorById(Long authorId);
+    Optional<Author> findAuthorByName(String name);
+}
