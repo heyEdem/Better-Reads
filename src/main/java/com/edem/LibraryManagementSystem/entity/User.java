e53c18d62f9de20ebeedc75ee6ed182e7ef1635c
heyEdem <akatoaugustine@gmail.com>
2023-11-01 00:28:19 +0000

    Columns and Basic

--- src/main/java/com/edem/LibraryManagementSystem/entity/User.java
+++ src/main/java/com/edem/LibraryManagementSystem/entity/User.java
@@ -3,6 +3,7 @@ package com.edem.LibraryManagementSystem.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Data
 @AllArgsConstructor
@@ -10,16 +11,41 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Entity
+@Table(name = "users")
 public class User {
     @Id
+    @Basic
     @Column(name = "user_id")
     private Long userId;
+
+    @Basic
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Basic
+    @Column(name = "password", nullable = false)
     private String password;
 
     @OneToOne(mappedBy = "user")
     private Author author;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(author, user.author);
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, email, password, author);
+    }
 //    @OneToOne(cascade = CascadeType.REMOVE)
 //    @JoinColumn(name = "user_id", referencedColumnName = "user_id",nullable = false)
 //    private User user;
