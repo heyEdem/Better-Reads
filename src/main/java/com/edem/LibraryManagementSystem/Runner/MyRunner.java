c33d9a30f9531b0488cfc89ef83c80e63b1e5b8b
heyEdem <akatoaugustine@gmail.com>
2023-11-03 16:50:24 +0000

    User Service

--- src/main/java/com/edem/LibraryManagementSystem/Runner/MyRunner.java
+++ src/main/java/com/edem/LibraryManagementSystem/Runner/MyRunner.java
@@ -1,17 +1,18 @@
-//package com.edem.LibraryManagementSystem.Runner;
-//
-//
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class MyRunner implements CommandLineRunner {
-//
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        User user1 = userService.createUser("user1@gmail.com","pass1");
-//        User user2 = userService.createUser("user2@gmail.com","pass2");
-//
-//    }
-//}
+package com.edem.LibraryManagementSystem.Runner;
+
+
+import com.edem.LibraryManagementSystem.entity.User;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRunner implements CommandLineRunner {
+    private final UserServiceImpl userService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user1 = userService.createUser("user1@gmail.com","pass1");
+        User user2 = userService.createUser("user2@gmail.com","pass2");
+
+    }
+}
---
+++ src/main/java/com/edem/LibraryManagementSystem/Service/UserService.java
@@ -0,0 +1,7 @@
+package com.edem.LibraryManagementSystem.Service;
+
+import com.edem.LibraryManagementSystem.entity.User;
+
+public interface UserService {
+     createUser(User user){}
+}
