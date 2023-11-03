011bab03775e35b4feed63ba387cf7b927b9512d
heyEdem <akatoaugustine@gmail.com>
2023-11-03 16:53:58 +0000

    User Service Impl

--- src/main/java/com/edem/LibraryManagementSystem/Service/UserService.java
+++ src/main/java/com/edem/LibraryManagementSystem/Service/UserService.java
@@ -3,5 +3,6 @@ package com.edem.LibraryManagementSystem.Service;
 import com.edem.LibraryManagementSystem.entity.User;
 
 public interface UserService {
-     createUser(User user){}
+     User createUser(User user);
+     void deleteUser(Long id);
 }
---
+++ src/main/java/com/edem/LibraryManagementSystem/Service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.edem.LibraryManagementSystem.Service.impl;
+
+import com.edem.LibraryManagementSystem.Service.UserService;
+import com.edem.LibraryManagementSystem.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public User createUser(User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+    }
+}
