3c6fe3a8a5bd18923fcfd67b0c6b47aa8b5dd679
heyEdem <akatoaugustine@gmail.com>
2023-11-03 16:55:57 +0000

    User Repository

---
+++ src/main/java/com/edem/LibraryManagementSystem/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.edem.LibraryManagementSystem.repository;
+
+import com.edem.LibraryManagementSystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
