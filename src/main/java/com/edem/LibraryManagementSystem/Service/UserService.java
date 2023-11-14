f5520d45bca56b6593599f2ec6380fbfab6d5950
heyEdem <akatoaugustine@gmail.com>
2023-11-14 23:33:35 +0000

    create user service method

--- src/main/java/com/edem/LibraryManagementSystem/Service/UserService.java
+++ src/main/java/com/edem/LibraryManagementSystem/Service/UserService.java
@@ -3,6 +3,6 @@ package com.edem.LibraryManagementSystem.Service;
 import com.edem.LibraryManagementSystem.entity.User;
 
 public interface UserService {
-     User createUser(User user);
+     User createUser(String email, String password);
      void deleteUser(Long id);
 }
