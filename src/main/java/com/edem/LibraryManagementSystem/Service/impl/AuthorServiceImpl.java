f0abf82b7338e264da5fe17dfcc0ef4b921827c2
heyEdem <akatoaugustine@gmail.com>
2023-12-02 23:47:39 +0000

    create Author without user

--- src/main/java/com/edem/LibraryManagementSystem/Service/impl/AuthorServiceImpl.java
+++ src/main/java/com/edem/LibraryManagementSystem/Service/impl/AuthorServiceImpl.java
@@ -3,8 +3,10 @@ package com.edem.LibraryManagementSystem.Service.impl;
 import com.edem.LibraryManagementSystem.Service.AuthorService;
 import com.edem.LibraryManagementSystem.entity.Author;
 import com.edem.LibraryManagementSystem.entity.Book;
+//import com.edem.LibraryManagementSystem.entity.User;
 import com.edem.LibraryManagementSystem.repository.AuthorRepository;
 import com.edem.LibraryManagementSystem.repository.BookRepository;
+//import com.edem.LibraryManagementSystem.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +16,27 @@ import java.util.Optional;
 import java.util.Set;
 
 @Service
-@RequiredArgsConstructor
 public class AuthorServiceImpl implements AuthorService {
     private AuthorRepository repository;
     private BookRepository bookRepository;
+//    private final UserRepository userRepository;
+
+    public AuthorServiceImpl(AuthorRepository repository, BookRepository bookRepository /*UserRepository userRepository*/) {
+        this.repository = repository;
+        this.bookRepository = bookRepository;
+//        this.userRepository = userRepository;
+    }
 
     @Override
-    public Author createAuthor(Author author) {
+    public Author createOrUpdateAuthor(Author author) {
         return repository.save(author);
+
+    }
+
+    @Override
+    public Author createAuthor(String name, String description /* Long userId*/) {
+//        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User with id " + userId + " not found"));
+        return repository.save(new Author(name, description /*user*/));
     }
 
     @Override
@@ -51,19 +66,13 @@ public class AuthorServiceImpl implements AuthorService {
 
     @Override
     public int bookCount(Author author) {
-//        var bookCount = new ArrayList<>();
-
-//        for (Book book: bookRepository.findAll())
-//            if(book.getAuthor().getId() == authorId) bookCount.add(book);
-//
-//        return bookCount.size();
-        var bookCount = repository.findAuthorBooks(author);
-        return bookCount.get().size();
+        return 0;
     }
 
     public Set<Book> findAuthorBooks(Long authorId){
         Optional<Author> author = repository.findById(authorId);
         Set<Book> list = author.get().getBooks();
+        int bookCount = list.size();
         return  list;
     }
 }
