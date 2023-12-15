055e48dee8ff0b31c4367599fdffdb82c2cbf03b
heyEdem <akatoaugustine@gmail.com>
2023-12-15 23:04:50 +0000

    AuthorServiceImpl CRUD

--- src/main/java/com/edem/LibraryManagementSystem/Service/impl/AuthorServiceImpl.java
+++ src/main/java/com/edem/LibraryManagementSystem/Service/impl/AuthorServiceImpl.java
@@ -2,42 +2,43 @@ package com.edem.LibraryManagementSystem.Service.impl;
 
 import com.edem.LibraryManagementSystem.Service.AuthorService;
 import com.edem.LibraryManagementSystem.entity.Author;
-import com.edem.LibraryManagementSystem.entity.Book;
-//import com.edem.LibraryManagementSystem.entity.User;
 import com.edem.LibraryManagementSystem.repository.AuthorRepository;
 import com.edem.LibraryManagementSystem.repository.BookRepository;
-//import com.edem.LibraryManagementSystem.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
-@Service
+@Service @Slf4j
 public class AuthorServiceImpl implements AuthorService {
-    private AuthorRepository repository;
-    private BookRepository bookRepository;
-//    private final UserRepository userRepository;
+    private final AuthorRepository repository;
+    private final BookRepository bookRepository;
 
-    public AuthorServiceImpl(AuthorRepository repository, BookRepository bookRepository /*UserRepository userRepository*/) {
+
+    public AuthorServiceImpl(AuthorRepository repository, BookRepository bookRepository ) {
         this.repository = repository;
         this.bookRepository = bookRepository;
-//        this.userRepository = userRepository;
     }
 
     @Override
-    public Author createOrUpdateAuthor(Author author) {
+    public Author createAuthor(Author author) {
+        author.setCreatedAt(LocalDateTime.now());
+        author.setLikes(BigInteger.valueOf(0));
+        log.info("new author created");
         return repository.save(author);
-
     }
-
     @Override
-    public Author createAuthor(String name, String description /* Long userId*/) {
-//        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User with id " + userId + " not found"));
-        return repository.save(new Author(name, description /*user*/));
+    public Optional<Author> findAuthorById(Long authorId) {
+        var author = repository.findById(authorId);
+        author.get().setBooks(bookRepository.findBookByAuthor(author.get()));
+        log.info("found author with id {}",author.get().getId());
+        return author;
     }
+
+
 
     @Override
     public List<Author> findAllAuthors() {
@@ -54,14 +55,12 @@ public class AuthorServiceImpl implements AuthorService {
         repository.findById(authorId).ifPresent(repository::delete);
     }
 
-    @Override
-    public Optional<Author> findAuthorById(Long authorId) {
-        return repository.findById(authorId);
-    }
 
     @Override
-    public Optional<Author> findAuthorByName(String lastName) {
-        return Optional.ofNullable(repository.findAuthorByLastName(lastName));
+    public List<Author> findAuthorByName(String keyword) {
+        if (repository.findAuthorsByFirstNameContainsIgnoreCase(keyword).isEmpty())
+            return repository.findAuthorsByLastNameContainsIgnoreCase(keyword);
+        return repository.findAuthorsByFirstNameContainsIgnoreCase(keyword);
     }
 
     @Override
@@ -69,10 +68,4 @@ public class AuthorServiceImpl implements AuthorService {
         return 0;
     }
 
-    public Set<Book> findAuthorBooks(Long authorId){
-        Optional<Author> author = repository.findById(authorId);
-        Set<Book> list = author.get().getBooks();
-        int bookCount = list.size();
-        return  list;
-    }
 }
