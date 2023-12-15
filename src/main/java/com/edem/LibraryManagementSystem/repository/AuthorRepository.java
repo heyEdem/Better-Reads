abb064d2ea3a679896155906b6473ed3b54db5ba
heyEdem <akatoaugustine@gmail.com>
2023-12-15 23:02:33 +0000

    AuthorRepository - findAuthorsByFirstNameContainsIgnoreCase, findAuthorsByLastNameContainsIgnoreCase

--- src/main/java/com/edem/LibraryManagementSystem/repository/AuthorRepository.java
+++ src/main/java/com/edem/LibraryManagementSystem/repository/AuthorRepository.java
@@ -1,15 +1,14 @@
 package com.edem.LibraryManagementSystem.repository;
 
 import com.edem.LibraryManagementSystem.entity.Author;
-import com.edem.LibraryManagementSystem.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author,Long> {
-    Author findAuthorByLastName(String lastName);
-   Optional<List<Book>> findAuthorBooks(Author author);
+    List<Author> findAuthorsByFirstNameContainsIgnoreCase(String firstName);
+    List<Author> findAuthorsByLastNameContainsIgnoreCase(String lastName);
 }
